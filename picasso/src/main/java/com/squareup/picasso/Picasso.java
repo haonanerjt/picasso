716f2cb8ad18b1e4abfe9b8b2811524c890d673d
Dimitris Couchell-Koutsogiorgas <dimitris@squareup.com>
2016-05-17 14:59:49 -0700

    Perform null check for canceling requests

--- picasso/src/main/java/com/squareup/picasso/Picasso.java
+++ picasso/src/main/java/com/squareup/picasso/Picasso.java
@@ -203,11 +203,19 @@ public class Picasso {
 
   /** Cancel any existing requests for the specified target {@link ImageView}. */
   public void cancelRequest(ImageView view) {
+    // checkMain() is called from cancelExistingRequest()
+    if (view == null) {
+      throw new AssertionError("view cannot be null.");
+    }
     cancelExistingRequest(view);
   }
 
   /** Cancel any existing requests for the specified {@link Target} instance. */
   public void cancelRequest(Target target) {
+    // checkMain() is called from cancelExistingRequest()
+    if (target == null) {
+      throw new AssertionError("target cannot be null.");
+    }
     cancelExistingRequest(target);
   }
 
@@ -216,6 +224,10 @@ public class Picasso {
    * viewId}.
    */
   public void cancelRequest(RemoteViews remoteViews, int viewId) {
+    // checkMain() is called from cancelExistingRequest()
+    if (remoteViews == null) {
+      throw new AssertionError("remoteViews cannot be null.");
+    }
     cancelExistingRequest(new RemoteViewsAction.RemoteViewsTarget(remoteViews, viewId));
   }
 
